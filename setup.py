1d7f3b5bb9d8cf42a42f6fb1206fb826f7382180
Alex Holmes <alex.holmes@isotoma.com>
2014-11-25 15:12:10 +0000

    Prepare for release 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.0.1'
+version = '0.0.2'
 
 def fread(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
